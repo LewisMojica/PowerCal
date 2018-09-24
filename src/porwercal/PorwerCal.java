b362e19882a8e8e909e5d3813ec81d872cbb08b1
Lewis Mojica <mojicalewis@gmail.com>
2018-09-24 19:08:34 -0400

    Prueba de las bibliotecas en el main

--- src/porwercal/PorwerCal.java
+++ src/porwercal/PorwerCal.java
@@ -5,10 +5,15 @@ import event.*;
 public class PorwerCal {
 
     public static void main(String[] args) {
-        Date fecha = new Date(new Hour(30, 34), new Day(4, 3, 2018), new Hour(1, 0));
+        Date fecha = new Date(new Hour(9, 30), new Day(4, 3, 2018), new Period(1, 45));
         
         System.out.println(fecha.getHour().toStringFormat12());
         System.out.println(fecha.getHour().toStringFormat24());
+        
+        System.out.println(fecha.getDuration().toStringFormat24());
+        
+        System.out.println(fecha.getDay().toString());
+        
     }
 
 }
