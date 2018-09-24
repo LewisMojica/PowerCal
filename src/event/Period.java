32431cf1236fe72d13f69eeddce0ed2b5c1a2da6
Lewis Mojica <mojicalewis@gmail.com>
2018-09-24 19:05:42 -0400

    Se creo nueva clase Period la cual sirve para almacenar una cienta cantidad de tiempo

---
+++ src/event/Period.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package event;
+
+/**
+ *
+ * @author star_man
+ */
+public class Period extends Time{
+    
+    public Period(int hour, int minute) {
+        super(hour, minute);
+    }
+    
+}
