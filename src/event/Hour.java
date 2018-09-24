bb494b849c57f41837d5dcb75dde23ba808160bd
Lewis Mojica <mojicalewis@gmail.com>
2018-09-24 19:02:31 -0400

    Ahora la clase Hour hereda y Time, por lo que no es necesario que tenga ciertas funciones

--- src/event/Hour.java
+++ src/event/Hour.java
@@ -1,65 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package event;
 
-
-/*
-* the hour is in 24 format
+/**
+ *
+ * @author star_man
  */
-public class Hour {
-
-    private int hour;
-    private int minute;
-
+public class Hour extends Time{
+    
     public Hour(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
+        super(hour, minute);
     }
-
-    public String toStringFormat24() {
-        String out;    //this var will be return
-
-        // convert the hour to String
-        out = String.valueOf(hour) + ":" + String.valueOf(minute);
-        return out;
-    }
-
-    public String toStringFormat12() {
-        String out;     //this var will be return
-        
-            //conver the hour to String but in 12 hours format 
-            if (hour > 12) {
-                out = String.valueOf(hour - 12) + ":" + String.valueOf(minute) + " PM";
-            } else{
-                out = String.valueOf(hour) + ":" + String.valueOf(minute) + "AM";
-            }
-        
-        return out;
-    }
-
-    /**
-     * @return the hour
-     */
-    public int getHour() {
-        return hour;
-    }
-
-    /**
-     * @param hour the hour to set
-     */
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
-    /**
-     * @return the minute
-     */
-    public int getMinute() {
-        return minute;
-    }
-
-    /**
-     * @param minute the minute to set
-     */
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
+    
 }
