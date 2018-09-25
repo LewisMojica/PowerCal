89e0bd6e36604c1c91b27d7c1a54dd48567fdab1
Lewis Mojica <mojicalewis@gmail.com>
2018-09-25 12:34:21 -0400

    Se importan las clases por separados, de esa manera se evita importar clases innecesarios

--- src/porwercal/PorwerCal.java
+++ src/porwercal/PorwerCal.java
@@ -1,19 +1,22 @@
 package porwercal;
 
-import event.*;
+import event.Date;
+import event.Day;
+import event.Hour;
+import event.Period;
 
 public class PorwerCal {
 
     public static void main(String[] args) {
-        Date fecha = new Date(new Hour(9, 30), new Day(4, 3, 2018), new Period(1, 45));
-        
+        Date fecha = new Date("Math class", new Hour(9, 30), new Day(4, 3, 2018), new Period(1, 45));
+
         System.out.println(fecha.getHour().toStringFormat12());
         System.out.println(fecha.getHour().toStringFormat24());
-        
+
         System.out.println(fecha.getDuration().toStringFormat24());
-        
+
         System.out.println(fecha.getDay().toString());
-        
+
     }
 
 }
