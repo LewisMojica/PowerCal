c85c9d5cd649ca9e099cf9b9d2310d3b26eeb1ca
Lewis Mojica <mojicalewis@gmail.com>
2018-09-26 21:50:01 -0400

    Prueba de funcionamiento de ArrayList

--- src/porwercal/PorwerCal.java
+++ src/porwercal/PorwerCal.java
@@ -1,18 +1,33 @@
 package porwercal;
 
-import event.Day;
-import event.Event;
-import event.Hour;
-import event.Period;
 import static porwercal.Methods.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PorwerCal {
 
+    static ArrayList<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        Event evento = new Event("Math class", new Hour(9, 30), new Day(4, 3, 2018), new Period(1, 45));
+        Scanner in = new Scanner(System.in);
+        int i;
+        do {
+            System.out.println("Ingrese el nuevo Integer");
+            i = in.nextInt();
+            if (i != 0 && i != 42) {
+                addItem(i);
+            }
+            System.out.println("Hay" + list.size());
+        } while (i != 0 && i != 42);
+        if (i == 42) {
+            for (int e = list.size(); e != 0; e--) {
+                System.out.println("Objeto no. " + (-1 + e) + " es igual a >> " + list.get(e - 1));
+            }
+        }
+    }
 
-        printMainMenu();
-
+    private static void addItem(Integer i) {
+        list.add(i);
     }
 
 }
