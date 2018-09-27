ec89e83a22b130966a483e73c8dc79842e571f3c
Lewis Mojica <mojicalewis@gmail.com>
2018-09-26 20:38:27 -0400

    Se crea metodo newEvent el cual crea un objeto de tipo evento llamado event

--- src/porwercal/Methods.java
+++ src/porwercal/Methods.java
@@ -5,16 +5,26 @@
  */
 package porwercal;
 
+import java.util.Scanner;
+import event.*;
 /**
  *
  * @author star_man
  */
 class Methods {
+    
+    private Scanner in = new Scanner(System.in);
+    
     static void printMainMenu(){
         System.out.print("Opciones principales: \n\n"
                 + "-->  0. Salir\n"
                 + "-->  1. Crear nuevo evento\n"
                 + "-->  2. Ver evento\n"
                 + "-->  3. Eliminar evento (no soportado)\n");
+    }
+    
+    static void newEvent(String name, Hour hour, Day day, Period duration){
+        
+        Event evento = new Event(name,hour, day, duration);
     }
 }
