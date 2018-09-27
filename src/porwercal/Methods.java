2bb572194c5edf7b3a67debc50f801feb160c77f
Lewis Mojica <mojicalewis@gmail.com>
2018-09-26 22:32:41 -0400

    Se crean los eventos que se utilizan al entrar en la opcion 1

--- src/porwercal/Methods.java
+++ src/porwercal/Methods.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
  */
 class Methods {
 
-    static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Event> events_array = new ArrayList<>();
     static private Scanner in = new Scanner(System.in);
     static int selection;
 
@@ -35,7 +35,8 @@ class Methods {
                         menuSelection3();
                         break;
                     default:
-                        System.out.print(selection + " No es una opcion valida");
+                        System.out.println("< " + selection + " > No es una opcion valida\n"
+                                + "------------------------------");
                 }
             }
         } while (selection != 0);
@@ -49,13 +50,36 @@ class Methods {
                 + "-->  3. Eliminar evento (no soportado)\n");
     }
 
+    
+
+    private static void menuSelection1() {
+        System.out.print("Ingrese el nombre del nuevo evento\n"
+                + "> ");
+        String name = in.next();
+
+        System.out.print("Ingrese la hora del evento formato: <hora>:<minuto>\n"
+                + "> ");
+        Hour hour = stringToHour(in.next());
+
+        System.out.print("Ingrese la fecha del nuevo evento formato: <day>/<month>/<year>\n"
+                + "> ");
+        Day day = stringToDay(in.next());
+
+        System.out.print("Ingrese la duracion del nuevo evento formato: <horas>:<minutos>\n"
+                + "> ");
+        Period duration = stringToPeriod(in.next());
+        
+        //se agraga el nuevo evento al array
+        newEvent(name, hour, day, duration);
+        
+        System.out.println("Evento < " + name + " >creado");
+
+    }
+    
     static void newEvent(String name, Hour hour, Day day, Period duration) {
 
         Event evento = new Event(name, hour, day, duration);
-    }
-
-    private static void menuSelection1() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        events_array.add(evento);
     }
 
     private static void menuSelection2() {
@@ -65,4 +89,20 @@ class Methods {
     private static void menuSelection3() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    //convierte un String del formato hora:minuto a un objeto de tipo Hour
+    private static Hour stringToHour(String next) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //convierte un String del formato horas:minutos  a un objeto de tipo Period
+    private static Period stringToPeriod(String next) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //convierte un String del formato day/mes/year a un objeto de tipo Day
+    private static Day stringToDay(String next) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
