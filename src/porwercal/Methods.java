f3674802e78c4c1b88153e069e11410e95434fb0
Lewis Mojica <mojicalewis@gmail.com>
2018-09-26 21:54:53 -0400

    Se crea metodo initMenu para iniciar la interaccion con el usuario

--- src/porwercal/Methods.java
+++ src/porwercal/Methods.java
@@ -7,24 +7,35 @@ package porwercal;
 
 import java.util.Scanner;
 import event.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author star_man
  */
 class Methods {
-    
-    private Scanner in = new Scanner(System.in);
-    
-    static void printMainMenu(){
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static private Scanner in = new Scanner(System.in);
+    static int selection;
+
+    static void initMenu() {
+        do {
+            printMainMenu();
+            selection = in.nextInt();
+        } while (selection != 0);
+    }
+
+    static void printMainMenu() {
         System.out.print("Opciones principales: \n\n"
                 + "-->  0. Salir\n"
                 + "-->  1. Crear nuevo evento\n"
                 + "-->  2. Ver evento\n"
                 + "-->  3. Eliminar evento (no soportado)\n");
     }
-    
-    static void newEvent(String name, Hour hour, Day day, Period duration){
-        
-        Event evento = new Event(name,hour, day, duration);
+
+    static void newEvent(String name, Hour hour, Day day, Period duration) {
+
+        Event evento = new Event(name, hour, day, duration);
     }
 }
--- src/porwercal/PorwerCal.java
+++ src/porwercal/PorwerCal.java
@@ -1,33 +1,13 @@
 package porwercal;
 
 import static porwercal.Methods.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class PorwerCal {
 
-    static ArrayList<Integer> list = new ArrayList<>();
+    
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int i;
-        do {
-            System.out.println("Ingrese el nuevo Integer");
-            i = in.nextInt();
-            if (i != 0 && i != 42) {
-                addItem(i);
-            }
-            System.out.println("Hay" + list.size());
-        } while (i != 0 && i != 42);
-        if (i == 42) {
-            for (int e = list.size(); e != 0; e--) {
-                System.out.println("Objeto no. " + (-1 + e) + " es igual a >> " + list.get(e - 1));
-            }
-        }
+        initMenu();
     }
-
-    private static void addItem(Integer i) {
-        list.add(i);
-    }
-
 }
