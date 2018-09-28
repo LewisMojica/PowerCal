fc12f59118f75e31146586989e8c5fa60b4b0e0d
Lewis Mojica <mojicalewis@gmail.com>
2018-09-27 23:28:10 -0400

    -la varialbe 'selection' ahora es local -El metodo menuSelection2 ahora ya esta soportado -Se crean metodos 'menuSelection12' y 'menuSelection13'

--- src/porwercal/Methods.java
+++ src/porwercal/Methods.java
@@ -17,11 +17,12 @@ class Methods {
 
     static private ArrayList<Event> events_array = new ArrayList<>();
     static private Scanner in = new Scanner(System.in);
-    static private int selection;
 
     static void initMenu() {
+        int selection;
         do {
             printMainMenu();
+            System.out.print("> ");
             selection = Integer.parseInt(in.nextLine());
             if (selection != 0) {
                 switch (selection) {
@@ -50,8 +51,6 @@ class Methods {
                 + "-->  3. Eliminar evento (no soportado)\n");
     }
 
-    
-
     private static void menuSelection1() {
         System.out.print("Ingrese el nombre del nuevo evento\n"
                 + "> ");
@@ -60,7 +59,7 @@ class Methods {
         System.out.print("Ingrese la hora del evento formato: <hora>:<minuto>\n"
                 + "> ");
         String test = in.nextLine();
-        while (test.indexOf(" ") != -1){
+        while (test.indexOf(" ") != -1) {
             System.out.print("Sin espacios en blanco\n"
                     + "> ");
             test = in.nextLine();
@@ -70,7 +69,7 @@ class Methods {
         System.out.print("Ingrese la fecha del nuevo evento formato: <day>/<month>/<year>\n"
                 + "> ");
         test = in.nextLine();
-        while (test.indexOf(" ") != -1){
+        while (test.indexOf(" ") != -1) {
             System.out.print("Sin espacios en blanco\n"
                     + "> ");
             test = in.nextLine();
@@ -80,22 +79,38 @@ class Methods {
         System.out.print("Ingrese la duracion del nuevo evento formato: <horas>:<minutos>\n"
                 + "> ");
         test = in.nextLine();
-        while (test.indexOf(" ") != -1){
+        while (test.indexOf(" ") != -1) {
             System.out.print("Sin espacios en blanco\n"
-                    + "> ");
+                    + "--> ");
             test = in.nextLine();
         }
         Period duration = stringToPeriod(test);
-        
+
         //se agrega el nuevo evento al array
         newEvent(name, hour, day, duration);
-        
+
         System.out.println("Evento < " + name + " >creado");
 
     }
-    
+
     private static void menuSelection2() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.print("Menu ver evento:\n\n"
+                + "-->  0. Volver a menu principal\n"
+                + "-->  1. Ver todos los eventos\n"
+                + "-->  2. Ver evento especifico\n"
+                + "> ");
+        int selection = Integer.parseInt(in.nextLine());
+
+        switch (selection) {
+            case 0:
+                break;
+            case 1:
+                menuSelection12();
+                break;
+            case 2:
+                menuSelection13();
+
+        }
     }
 
     private static void menuSelection3() {
@@ -108,7 +123,7 @@ class Methods {
         int hour = Integer.parseInt(next.substring(0, colon)); //se guarda el valor numerico de las horas
         int minutes = Integer.parseInt(next.substring(colon + 1)); //se guarda el valor numerico de los minutos
         return new Hour(hour, minutes);
-        
+
     }
 
     //convierte un String del formato horas:minutos  a un objeto de tipo Period
@@ -123,16 +138,24 @@ class Methods {
     private static Day stringToDay(String next) {
         int slash_1 = next.indexOf("/"); //ubacar la posicion de la primera ocurrencia del char '/'
         int slash_2 = next.indexOf("/", slash_1 + 1);//ubacar la posicion de la segunda ocurrencia del char '/'
-        int day = Integer.parseInt(next.substring(0,slash_1));//asigna el dia a la variable Day
+        int day = Integer.parseInt(next.substring(0, slash_1));//asigna el dia a la variable Day
         int month = Integer.parseInt(next.substring(slash_1 + 1, slash_2));//asigna el mes a la variable month
         int year = Integer.parseInt(next.substring(slash_2 + 1));//so on
         return new Day(day, month, year);
     }
-    
+
     private static void newEvent(String name, Hour hour, Day day, Period duration) {
 
         Event evento = new Event(name, hour, day, duration);
         events_array.add(evento);
+    }
+
+    private static void menuSelection12() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void menuSelection13() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
